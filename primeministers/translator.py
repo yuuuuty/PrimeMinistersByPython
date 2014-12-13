b39f90c79c8570e916a657df3273c1ef51798b33
yuto ueda <g1244163@cse.kyoto-su.ac.jp>
2014-12-14 07:58:18 +0900

    yuto html完成 2014/12/14
    makeしたら、実行できると思うから、実行してみて。

--- primeministers/translator.py
+++ primeministers/translator.py
@@ -13,16 +13,67 @@ class Translator(object):
 
 	def __init__(self, input_table):
 		"""トランスレータのコンストラクタ。"""
+		self._input_table = input_table
+		self._output_table = table.Table('output')
+
 		return
 
 	def compute_string_of_days(self, period):
 		"""在位日数を計算して、それを文字列にして応答する。"""
-		return None
+		if period != "在位期間" :
+			a_date = re.split(r'\D+', period)
+			a_date.remove('')
+			# print a_date
+			start_day = datetime.date(int(a_date[0]),int(a_date[1]),int(a_date[2]))
+			if(len(a_date) == 3):
+				current_date = datetime.date.today()
+				end_day = datetime.date(current_date.year, current_date.month, current_date.day)
+			else:
+				end_day = datetime.date(int(a_date[3]),int(a_date[4]),int(a_date[5]))
+			priod_days = end_day - start_day
+
+			return re.sub(r'(\d)(?=(\d{3})+(?!\d))', '\\1,', str(priod_days.days+1))
+		else:
+			return "在位日数"
+
 
 	def compute_string_of_image(self, tuple):
 		"""サムネイル画像から画像へ飛ぶためのHTML文字列を作成して、それを応答する。"""
-		return None
+
+		values = tuple.values()
+		alt = values[8].split("images/")
+		if values[self._input_table.attributes().keys().index('image')] == '画像':
+			return '画像'
+		print alt
+		html_img_str = "<a name=\""+values[0]+"\" href=\""+values[8]+"\"><img class=\"borderless\" src=\""+values[9]+"\" width=\"25\" height=\"32\" alt=\""+alt[0]+"\"></a>"
+		return html_img_str
 
 	def table(self):
 		"""総理大臣のCSVファイルを基にしたテーブルから、HTMLページを基にするテーブルに変換して、それを応答する。"""
-		return None
+		html_table = []
+		attributes = self._input_table.attributes()
+		keys = attributes.keys()
+		tuples = self._input_table.tuples()
+		values = []
+
+		for index,a_tuple in enumerate(tuples):
+			values = a_tuple.values()
+
+			html_table.append([])
+			html_table[index].append(values[keys.index("no")])
+			html_table[index].append(values[keys.index("order")])
+			html_table[index].append(values[keys.index("names")])
+			html_table[index].append(values[keys.index("kana")])
+			html_table[index].append(values[keys.index("period")])
+			html_table[index].append(self.compute_string_of_days(values[keys.index("period")]))
+			html_table[index].append(values[keys.index("school")])
+			html_table[index].append(values[keys.index("party")])
+			html_table[index].append(values[keys.index("birth")])
+			html_table[index].append(self.compute_string_of_image(a_tuple))
+
+		print len(html_table)
+
+		for row in html_table:
+			self._output_table.add(tuple.Tuple(self._output_table.attributes(),row))
+
+		return self._output_table
--- primeministers/writer.py
+++ primeministers/writer.py
@@ -29,6 +29,22 @@ class Writer(io.IO):
 
 	def write_body(self, file):
 		"""ボディを書き出す。つまり、属性リストを書き出し、タプル群を書き出す。"""
+		tuples = self._table.tuples()
+		keys = self._table.attributes().keys()
+		body_str = ""
+
+		for index, a_tuple in enumerate(tuples) :
+			body_str = body_str+"\t\t\t\t\t\t<tr>\n"
+			for value in a_tuple.values():
+				if index == 0:
+					body_str = body_str+"\t\t\t\t\t\t<td class=\"center-pink\"><string>"+value+"</string></td>\n"
+				elif index % 2 == 0:
+					body_str = body_str+"\t\t\t\t\t\t<td class=\"center-yellow\">"+value+"</td>\n"
+				else :
+					body_str = body_str+"\t\t\t\t\t\t<td class=\"center-blue\">"+value+"</td>\n"
+			body_str = body_str+"\t\t\t\t\t\t<tr>\n"
+
+		file.write(body_str)
 		return
 
 	def write_footer(self, file):
@@ -38,7 +54,7 @@ class Writer(io.IO):
 		time_string = datetime.datetime.now().strftime("%H:%M:%S")
 		file.write("\t\t\t\t\t</tbody>\n\t\t\t\t</table>\n\t\t\t</td>\n\t\t</tr>\n\t</tbody>\n</table>\n<hr>\n<div class=\"right-small\">Created using " + name_string + " " + date_string + " " + time_string + "</div>\n</body>\n</html>\n")
 		return
-	
+
 	def write_header(self, file):
 		"""ヘッダを書き出す。"""
 		file.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n<html lang=\"ja\">\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n<meta http-equiv=\"Content-Style-Type\" content=\"text/css\">\n<meta http-equiv=\"Content-Script-Type\" content=\"text/javascript\">\n<meta name=\"keywords\" content=\"Smalltalk,Oriented,Programming\">\n<meta name=\"description\" content=\"Prime Ministers\">\n<meta name=\"author\" content=\"AOKI Atsushi\">\n<link rev=\"made\" href=\"http://www.cc.kyoto-su.ac.jp/~atsushi/\">\n<link rel=\"index\" href=\"index.html\">\n<style type=\"text/css\">\n<!--\nbody {\n\tbackground-color : #ffffff;\n\tmargin : 20px;\n\tpadding : 10px;\n\tfont-family : serif;\n\tfont-size : 10pt;\n}\na {\n\ttext-decoration : underline;\n\tcolor : #000000;\n}\na:link {\n\tbackground-color : #ffddbb;\n}\na:visited {\n\tbackground-color : #ccffcc;\n}\na:hover {\n\tbackground-color : #dddddd;\n}\na:active {\n\tbackground-color : #dddddd;\n}\ndiv.belt {\n\tbackground-color : #eeeeee;\n\tpadding : 0px 4px;\n}\ndiv.right-small {\n\ttext-align : right;\n\tfont-size : 8pt;\n}\nimg.borderless {\n\tborder-width : 0px;\n\tvertical-align : middle;\n}\ntable.belt {\n\tborder-style : solid;\n\tborder-width : 0px;\n\tborder-color : #000000;\n\tbackground-color : #ffffff;\n\tpadding : 0px 0px;\n\twidth : 100%;\n}\ntable.content {\n\tborder-style : solid;\n\tborder-width : 0px;\n\tborder-color : #000000;\n\tpadding : 2px 2px;\n}\ntd.center-blue {\n\tpadding : 2px 2px;\n\ttext-align : center;\n\tbackground-color : #ddeeff;\n}\ntd.center-pink {\n\tpadding : 2px 2px;\n\ttext-align : center;\n\tbackground-color : #ffddee;\n}\ntd.center-yellow {\n\tpadding : 2px 2px;\n\ttext-align : center;\n\tbackground-color : #ffffcc;\n}\n-->\n</style>\n<title>Prime Ministers</title>\n</head>\n<body>\n<div class=\"belt\">\n<h2>Prime Ministers</h2>\n</div>\n<table class=\"belt\" summary=\"table\">\n\t<tbody>\n\t\t<tr>\n\t\t\t<td>\n\t\t\t\t<table class=\"content\" summary=\"table\">\n\t\t\t\t\t<tbody>\n")
