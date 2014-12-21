5a5ba96da641c8a0ed77fda14d2051803b2bf6b7
yuto ueda <g1244163@cse.kyoto-su.ac.jp>
2014-12-22 02:14:35 +0900

    auto 12/22 translator完成
    トランスレータが完成し、アウトプットテーブルを作成できました。computeNumberOfImageの引数を減らしてしまったので、注意お願いし
    ます。（特に、デザイナ）

--- PrimeMinistersByJava/primeministers/Translator.java
+++ PrimeMinistersByJava/primeministers/Translator.java
@@ -1,7 +1,11 @@
 package primeministers;
 
 import javax.swing.JOptionPane;
-import java.io.File;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * トランスレータ：総理大臣のCSVファイルをHTMLページへと変換するプログラム。
@@ -39,17 +43,18 @@ public class Translator extends Object
 		Downloader aDownloader = new Downloader();
 		aDownloader.downloadCSV();
 
-		System.out.println("[Translator]一時的にファイルをローカルから読み込みます");
-		File CSVFile= new File("./PrimeMinisters.csv");
-		
-		System.out.println("[Translator]CSVファイル読み込み完了 \""+CSVFile+"\"");
 		this.inputTable = aDownloader.table();
-		//System.out.println("inputTable: \n"+inputTable);
-		
+		// System.out.println("inputTable: \n"+inputTable);
+
 		aDownloader.downloadImages();
 		aDownloader.downloadThumbnails();
-		
-		
+
+		System.out.println("[Translator]Inputテーブル作成完了");
+		// System.out.println("inputTable: \n" + this.inputTable);
+
+		this.outputTable = table(this.inputTable);
+		System.out.println("[Translator]Outputテーブル作成完了");
+		System.out.println("outputTable: \n" + this.outputTable);
 
 		String aString = "総理大臣のCSVファイルからHTMLページへの変換を無事に完了しました。\n";
 		JOptionPane.showMessageDialog(null, aString, "報告",
@@ -62,15 +67,64 @@ public class Translator extends Object
 	 */
 	public String computeNumberOfDays(String periodString)
 	{
-		return null;
+		System.out.println(periodString);
+		if (!periodString.equals("在位期間"))
+		{
+			String[] aDate = periodString.split("\\D");
+			String startDayStr = aDate[0] + "/" + aDate[1] + "/" + aDate[2];
+			String endDayStr = "";
+			long periodDays = 0;
+
+			try
+			{
+				if (aDate.length == 7)
+				{
+					endDayStr = aDate[4] + "/" + aDate[5] + "/" + aDate[6];
+				}
+
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+				Date startDay = sdf.parse(startDayStr);
+				Date endDay;
+				if (aDate.length != 7)
+					endDay = new Date();
+				else
+					endDay = sdf.parse(endDayStr);
+
+				periodDays = (endDay.getTime() - startDay.getTime())
+				        / (1000 * 60 * 60 * 24) + 1;
+
+			}
+			catch (ParseException e)
+			{
+				e.printStackTrace();
+			}
+
+			String commaPeriodDays = String.valueOf(periodDays).replaceAll(
+			        "(\\d)(?=(\\d{3})+(?!\\d))", "\\2,");
+			return commaPeriodDays;
+		}
+		else
+		{
+			return "在位日数";
+		}
 	}
 
 	/**
 	 * サムネイルが画像から画像へ飛ぶためのHTML文字列を作成して、それを応答する。
 	 */
-	public String computeNumberOfImage(String periodString, Tuple aTuple, int no)
+	public String computeNumberOfImage(String periodString, Tuple aTuple)
 	{
-		return null;
+		ArrayList<String> values = aTuple.values();
+		String[] alt = periodString.split("images/");
+		if (values.get(aTuple.attributes().keys().indexOf("image"))
+		        .equals("画像"))
+			return "画像";
+
+		String htmlImgStrings = "<a name=\"" + values.get(0) + "\" href=\""
+		        + values.get(8) + "\">" + "<img class=\"borderless\" src=\""
+		        + values.get(9) + "\"" + " width=\"25\" height=\"32\" alt=\""
+		        + alt[1] + "\"></a>";
+		return htmlImgStrings;
 	}
 
 	/**
@@ -78,7 +132,38 @@ public class Translator extends Object
 	 */
 	public Table table(Table aTable)
 	{
-		return null;
-	}
+		System.out.println("[Translator]tableの起動を確認");
 
+		Table htmlTable = new Table();
+		Attributes htmlAttributes = new Attributes("output");
+
+		ArrayList<String> keys = aTable.attributes().keys();
+		ArrayList<Tuple> tuples = aTable.tuples();
+		ArrayList<String> values;
+
+		for (Tuple aTuple : tuples)
+		{
+			values = aTuple.values();
+			Tuple htmlTuple = new Tuple(htmlAttributes, new ArrayList<String>());
+
+			htmlTuple.values().add(values.get(keys.indexOf("no")));
+			htmlTuple.values().add(values.get(keys.indexOf("order")));
+			htmlTuple.values().add(values.get(keys.indexOf("names")));
+			htmlTuple.values().add(values.get(keys.indexOf("kana")));
+			htmlTuple.values().add(values.get(keys.indexOf("period")));
+			htmlTuple.values()
+			        .add(this.computeNumberOfDays(values.get(keys
+			                .indexOf("period"))));
+			htmlTuple.values().add(values.get(keys.indexOf("school")));
+			htmlTuple.values().add(values.get(keys.indexOf("party")));
+			htmlTuple.values().add(values.get(keys.indexOf("birth")));
+			htmlTuple.values().add(
+			        this.computeNumberOfImage(
+			                values.get(keys.indexOf("image")), aTuple));
+
+			htmlTable.add(htmlTuple);
+		}
+
+		return htmlTable;
+	}
 }
